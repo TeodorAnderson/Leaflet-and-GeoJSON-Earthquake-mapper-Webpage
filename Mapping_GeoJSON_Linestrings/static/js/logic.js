cf3b38163d3e9b4087beea619602f0fdcd22cea5
Teodor Anderson <teodoranderson@Teodors-MBP.lan>
2023-03-09 21:23:42 -0800

    last commit

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,40 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+// Create the map object with center at the San Francisco airport.
+// Create the map object with center and zoom level.
+
+
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+//We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+let baseMaps = {
+  Light: light,
+  Dark: dark
+};
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [light]
+});
+L.control.layers(baseMaps).addTo(map);
+let torontoData = "https://raw.githubusercontent.com//TeodorAnderson/Leaflet-and-GeoJSON-Earthquake-mapper-Webpage/main/torontoRoutes.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
+
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
